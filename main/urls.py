141fcd61d0900dcbc79a5a80cb024b504b983b04
fariskafedira <fariska.fedira@gmail.com>
2023-10-27 22:25:42 +0700

    update register

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from main.views import main, profile_user
+from main.views import main, profile_user, register
 
 app_name = 'main'
 
 urlpatterns = [
     path('', main, name='main'),
     path('profile-user/', profile_user, name='profile_user'),
+    path('register/', register, name='register'),
 ]
