b2b650ff722c6fb681a2366f96a5070e8a5d4362
febrian-irv <febrian.irv@gmail.com>
2023-10-28 14:17:16 +0700

    styling and routing profile

--- tales_tails_cafe/settings.py
+++ tales_tails_cafe/settings.py
@@ -126,6 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'main.User'
