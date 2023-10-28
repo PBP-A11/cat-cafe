7393196e929b81a02cf754559fcf41d8fb68c34a
mhmmdmadhanip <muhammad.madhani@ui.ac.id>
2023-10-29 04:44:17 +0700

    Menyelesaikan module detail buku

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from catalog.views import catalog, search_books, get_books_json
+from catalog.views import catalog, search_books, get_books_json, book_borrowed
 
 app_name = 'catalog'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', catalog, name='catalog'),
     path("search/<str:query>", search_books, name='search'),
     path("get-books/", get_books_json, name='get_books_json'),
+    path("borrow-book/<int:id>", book_borrowed, name='borrow_book')
 ]
