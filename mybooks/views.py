b313d36a93b71f597310d69dbe725f5e012dff7e
Athar Adista <athar0165@gmail.com>
2023-10-29 21:30:52 +0700

    finish homepage

--- mybooks/views.py
+++ mybooks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required, login_required
 from main.models import User, Book
@@ -22,9 +22,23 @@ def mybooks(request):
         }
         return render(request, 'listpeminjam.html', context)
     else :
-        return render(request, "mybooks.html")
+        books = Book.objects.filter(borrower=request.user)
+        context ={
+            'books': books
+        }
+        return render(request, "mybooks.html", context)
+        
 
 def get_mybooks_json(request):
     data = Book.objects.filter(borrower=request.user)
     return HttpResponse(serializers.serialize('json', data),
         content_type="application/json")
+
+def book_return(request, id):
+    if request.method == 'GET':
+        data = Book.objects.get(pk=id)
+        data.is_borrowed = False
+        data.borrower = None
+        data.save()
+        return HttpResponse(b"SUCCESS", status=201)
+    return HttpResponseNotFound()
