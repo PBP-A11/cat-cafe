5468f5dc8bce511b9e0ff38880736cbff7434c80
febrian-irv <febrian.irv@gmail.com>
2023-10-29 11:03:25 +0700

    implement edit profile ajax (blm style)

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from user_profile.views import user_profile
+from user_profile.views import user_profile, get_user_json, edit_profile
+
+
+app_name = 'user_profile'
 
 urlpatterns = [
     path('', user_profile, name='user_profile'),
+    path('get-user/', get_user_json, name='get_user_json'),
+    path('edit-profile/', edit_profile, name='edit_profile'),
 ]
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from main.models import Book, User
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseNotFound, JsonResponse
+from django.core import serializers
 
 @login_required(login_url='/login')
 def user_profile(request):
@@ -22,3 +24,27 @@ def user_profile(request):
     }
     
     return render(request, 'member_profile.html', context)
+
+def get_user_json(request):
+    user = request.user
+    user_data = {
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'username': user.username,
+        'email': user.email,
+        'date_of_birth': user.date_of_birth,
+    }
+    return JsonResponse(user_data)
+
+def edit_profile(request):
+    if request.method == 'POST':
+        user = request.user
+        user.first_name = request.POST.get("first_name")
+        user.last_name = request.POST.get("last_name")
+        user.username = request.POST.get("username")
+        user.date_of_birth = request.POST.get("date_of_birth")
+        user.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
