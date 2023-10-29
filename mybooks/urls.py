ffa45451f32400556d175634b480a078ea43e3ea
febrian-irv <febrian.irv@gmail.com>
2023-10-29 18:43:11 +0700

    done implement return book in mybooks

--- mybooks/urls.py
+++ mybooks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from mybooks.views import mybooks, get_mybooks_json
+from mybooks.views import mybooks, get_mybooks_json, book_return
 
 app_name = 'mybooks'
 
 urlpatterns = [
     path('', mybooks, name='mybooks'),
     path('get-mybooks/', get_mybooks_json, name='get_mybooks_json'),
+    path('book-return/<int:id>', book_return, name='book_return'),
 ]
